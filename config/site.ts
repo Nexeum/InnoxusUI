34dc978ce78d409de478468f69b424643c13c132
Nexeum <srandres1803@gmail.com>
2024-05-11 09:42:36 -0500

    feat: change site config

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-    name: "Next.js + NextUI",
-    description: "Make beautiful websites regardless of your design experience.",
+    name: "Innoxus",
+    description: "Dynamic orchestration: agile and responsive",
     navItems: [
         {
             label: "Home",
