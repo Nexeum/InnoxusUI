facc19b17ef49a707e8d51849083235b4980f6bf
Nexeum <srandres1803@gmail.com>
2024-05-14 21:49:57 -0500

    fix: refactor code

--- app/raw/layout.tsx
+++ app/raw/layout.tsx
@@ -1,8 +1,4 @@
-export default function RawLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RawLayout({ children }: { children: React.ReactNode }) {
   return (
     <section>
       <div>{children}</div>
