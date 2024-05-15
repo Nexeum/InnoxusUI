9717467a5baaffe63277f2067dc4ecb98fee80d4
Nexeum <srandres1803@gmail.com>
2024-05-14 21:51:21 -0500

    fix: refactor code

--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -1,13 +1,11 @@
 export default function RegisterLayout({
-                                          children,
-                                      }: {
-    children: React.ReactNode;
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <section>
-            <div>
-                {children}
-            </div>
-        </section>
-    );
+  return (
+    <section>
+      <div>{children}</div>
+    </section>
+  );
 }
