7063c325ef3874dea109f6737e7916ec1a5a4b6d
Nexeum <srandres1803@gmail.com>
2024-05-16 17:05:15 -0500

    feat: add auth service

---
+++ app/domain/usecase/auth/authService.tsx
@@ -0,0 +1,46 @@
+export async function login(email: string, password: string) {
+  const response = await fetch("http://localhost:8089/v1/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.text();
+    throw new Error(errorData || `HTTP error! status: ${response.status}`);
+  }
+
+  const jwt = await response.text();
+  document.cookie = `jwt=${jwt}; path=/; max-age=3600`;
+  return "Login successful";
+}
+
+export async function register(
+  username: string,
+  email: string,
+  password: string
+) {
+  const response = await fetch("http://localhost:8089/v1/auth/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.text();
+    throw new Error(errorData || `HTTP error! status: ${response.status}`);
+  }
+
+  return response;
+}
