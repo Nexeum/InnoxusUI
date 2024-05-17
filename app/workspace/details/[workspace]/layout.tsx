d5872cd196e7aa37d5293746c69d5434c1e33fd2
Nexeum <srandres1803@gmail.com>
2024-05-16 20:38:34 -0500

    feat: add layout workspace

---
+++ app/workspace/details/[workspace]/layout.tsx
@@ -0,0 +1,12 @@
+export default function DetailsLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <section>
+        <div>{children}</div>
+      </section>
+    );
+  }
+  
