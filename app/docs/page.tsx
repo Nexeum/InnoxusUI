85a1c99f42a0ff1cf8a55a857587a4393279e37a
Nexeum <srandres1803@gmail.com>
2024-05-17 14:59:57 -0500

    feat: add link download

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,16 +1,43 @@
+import { AppleIcon, DockIcon, LinuxIcon } from "@/components/icons";
+import Link from "next/link";
+
 export default function DocsPage() {
   return (
     <main className="mx-auto max-w-6xl">
-      <div className="space-y-2">
-        <div className="space-y-4">
-          <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl">
-            INX Version Control
-          </h1>
-          <p className="text-lg text-gray-500 dark:text-gray-400">
-            Manage your code with the powerful distributed version control
-            system.
-          </p>
+      <div className="mx-auto text-center">
+        <div className="space-y-2">
+          <div className="space-y-4">
+            <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl">
+              INX Version Control
+            </h1>
+            <p className="text-lg text-gray-500 dark:text-gray-400">
+              Manage your code with the powerful distributed version control
+              system.
+            </p>
+          </div>
         </div>
+        <section className="w-full mt-12">
+          <div className="container mx-auto">
+            <div className="space-y-4">
+              <div className="mx-auto">
+                <Link
+                  className="inline-flex h-10 items-center justify-center rounded-md mr-4 bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+                  href="#"
+                >
+                  <AppleIcon className="mr-2 h-5 w-5" />
+                  Download for Mac
+                </Link>
+                <Link
+                  className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300 dark:border-gray-800"
+                  href="#"
+                >
+                  <DockIcon className="mr-2 h-5 w-5" />
+                  Download for Linux
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
       <div className="mt-12 space-y-12">
         <section className="space-y-6" id="getting-started">
