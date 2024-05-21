163a922cb51d490c256bbe4b879b97a007b2371b
Nexeum <srandres1803@gmail.com>
2024-05-20 19:27:22 -0500

    fix: configure buttons download

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import { AppleIcon, GithubIcon, LaptopIcon } from "@/components/icons";
 
 export default function Home() {
   return (
@@ -19,34 +19,34 @@ export default function Home() {
         </h2>
       </div>
 
-      <div className="flex gap-3">
+      <div className="mt-2">
+        <Snippet hideSymbol hideCopyButton variant="flat">
+          <span>
+            Dont forget install <Code color="primary">inx</Code>
+          </span>
+        </Snippet>
+      </div>
+
+      <div className="flex gap-3 mt-4">
         <Link
           isExternal
           href={siteConfig.links.docs}
           className={buttonStyles({
-            color: "primary",
+            color: "default",
             radius: "full",
-            variant: "shadow",
           })}
         >
-          Documentation
+          <AppleIcon size={20} />
+          Download for MacOs
         </Link>
         <Link
           isExternal
           className={buttonStyles({ variant: "bordered", radius: "full" })}
           href={siteConfig.links.github}
         >
-          <GithubIcon size={20} />
-          GitHub
+          <LaptopIcon size={20} />
+          Download for Linux
         </Link>
-      </div>
-
-      <div className="mt-8">
-        <Snippet hideSymbol hideCopyButton variant="flat">
-          <span>
-            Dont forget VCS <Code color="primary">inx</Code>
-          </span>
-        </Snippet>
       </div>
     </section>
   );
