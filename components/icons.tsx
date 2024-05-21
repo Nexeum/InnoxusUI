ddee7294963290e3bdf46c47e049fce981336e3a
Nexeum <srandres1803@gmail.com>
2024-05-20 19:27:49 -0500

    feat: add laptop icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -635,6 +635,30 @@ export const MemoryStickIcon: React.FC<IconSvgProps> = ({
   );
 };
 
+export const LaptopIcon: React.FC<IconSvgProps> = ({
+  size = 24,
+  width,
+  height,
+  ...props
+}) => {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M20 16V7a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v9m16 0H4m16 0 1.28 2.55a1 1 0 0 1-.9 1.45H3.62a1 1 0 0 1-.9-1.45L4 16" />
+    </svg>
+  );
+};
+
 export const ImageIcon: React.FC<IconSvgProps> = ({
   size = 24,
   width,
