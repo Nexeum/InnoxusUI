86093939875c6e92a41971659d56141e9f25aac1
Nexeum <srandres1803@gmail.com>
2024-05-24 15:23:12 -0500

    fix: remove GitHub Icon

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { AppleIcon, GithubIcon, LaptopIcon } from "@/components/icons";
+import { AppleIcon, LaptopIcon } from "@/components/icons";
 
 export default function Home() {
   return (
